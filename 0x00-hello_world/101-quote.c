d72a016d07661bc7ad64daac5d02568d9ec1ee81
MentalStrang <mohamed.ramadan2393@gmail.com>
2023-03-18 04:18:13 +0200

    d

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,8 +9,8 @@
  */
 int main(void)
 {
-    char *print = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char *print = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-    write(STDOUT_FILENO, print, strlen(print));
-    return (1);
+	write(STDOUT_FILENO, print, strlen(print));
+	return (1);
 }
