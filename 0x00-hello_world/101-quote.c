efbb607717fbaec59fd9f7479a11a7fe102dfa9d
MentalStrang <mohamed.ramadan2393@gmail.com>
2023-03-18 04:17:20 +0200

    adv task 2

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+/**
+ * main - entry
+ *
+ * Return: 1
+ */
+int main(void)
+{
+    char *print = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+    write(STDOUT_FILENO, print, strlen(print));
+    return (1);
+}
